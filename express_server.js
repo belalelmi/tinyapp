aa0c8bb81c35b9fd953498b30040220a100e3646
belalelmi <belalelmi@gmail.com>
2021-11-08 15:22:57 -0500

    Adding Routes.

--- express_server.js
+++ express_server.js
@@ -10,6 +10,12 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
