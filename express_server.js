5b16ea46c6462fe827b9330e07fcd330d7b06820
belalelmi <belalelmi@gmail.com>
2021-11-09 14:03:51 -0500

    testing new shortURLs route

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-app.set("view engine", "ejs")
 const PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,9 +22,16 @@ app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
 });
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
+app.get('/urls', (req, res) => {
+  const templateVars = {
+    urls: urlDatabase
+  };
+  res.render('urls_index', templateVars)
+})
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  console.log(req.params.shortURL)
+  res.render("urls_show", templateVars);
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
