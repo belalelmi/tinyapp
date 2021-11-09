533697555b9b1dc295aa56795fd212b1bd9c0252
belalelmi <belalelmi@gmail.com>
2021-11-09 16:58:45 -0500

    functionality fixes

--- express_server.js
+++ express_server.js
@@ -3,17 +3,24 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+};
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+app.listen(PORT, () => {
+  console.log(`TinyApp listening on port ${PORT}!`);
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
@@ -28,11 +35,24 @@ app.get('/urls', (req, res) => {
   };
   res.render('urls_index', templateVars)
 })
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   console.log(req.params.shortURL)
   res.render("urls_show", templateVars);
 });
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL
+  urlDatabase[shortURL] = longURL
+  console.log(req.body);  // Log the POST request body to the console
+  res.redirect(`/urls/${shortURL}`)       // Respond with 'Ok' (we will replace this)
 });
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
