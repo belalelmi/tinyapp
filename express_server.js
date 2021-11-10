ce4dcdb6895d4e4e8d486ad55b5c49545fed812f
belalelmi <belalelmi@gmail.com>
2021-11-10 18:02:25 -0500

    fixed update route

--- express_server.js
+++ express_server.js
@@ -12,7 +12,7 @@ const urlDatabase = {
 };
 function generateRandomString() {
   return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
-};
+}
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -25,34 +25,45 @@ app.listen(PORT, () => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
 app.get('/urls', (req, res) => {
   const templateVars = {
     urls: urlDatabase
   };
-  res.render('urls_index', templateVars)
-})
+  res.render('urls_index', templateVars);
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  console.log(req.params.shortURL)
+  // console.log(req.params.shortURL + ' was just created!')
   res.render("urls_show", templateVars);
 });
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
-  const longURL = req.body.longURL
-  urlDatabase[shortURL] = longURL
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
   console.log(req.body);  // Log the POST request body to the console
-  res.redirect(`/urls/${shortURL}`)       // Respond with 'Ok' (we will replace this)
+  res.redirect('/urls');       // Respond with 'Ok' (we will replace this)
 });
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  console.log(req.params.shortURL + " was just DELETED!!");
+  res.redirect("/urls");
+});
+app.post("/urls/:shortURL/", (req, res) => {
+  // delete urlDatabase[req.params.shortURL];
+  // console.log(req.params.shortURL)
+  const longURL = req.body.longURL;
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = longURL;
+  // console.log(urlDatabase[shortURL])
+  res.redirect('/urls');
 });
 
